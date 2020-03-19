cb69c68469ac7b5ab646fb9bc10e3bff88d75b23
strunov <sergey.trunov@dataart.com>
2020-03-19 15:50:34 +0300

    Initial commit

---
+++ source/template.c
@@ -0,0 +1,36 @@
+
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//---------------------------------------------------------------------------------
+// Program entry point
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+
+
+	// the vblank interrupt must be enabled for VBlankIntrWait() to work
+	// since the default dispatcher handles the bios flags no vblank handler
+	// is required
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	consoleDemoInit();
+
+	// ansi escape sequence to set print co-ordinates
+	// /x1b[line;columnH
+    iprintf("\x1b[3;15HCYKA!");
+    iprintf("\x1b[2;10HWorld");
+	iprintf("\x1b[1;5HHello");
+
+	while (1) {
+		VBlankIntrWait();
+	}
+}
+
+
